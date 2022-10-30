31e43cb92cfefd453ddd2ae6170655ec283da54d
mahfuz <20mahfuzahmed20@gmail.com>
2022-10-30 13:41:07 -0400

    first commit

---
+++ src/com/bw/test/Java1_Whitebox.java
@@ -0,0 +1,74 @@
+package com.bw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Java1_Whitebox {
+
+	public double func01(int a, int b, double c) {
+		if (a > 10 && b < 10) {
+			a = a + b;
+			return a;
+		} else if (c > 0 || a > 100) {
+			c = c + 10;
+			return c;
+		} else {
+			return b = 10 + a;
+		}
+	}
+
+	@Test
+	public void test_Func01() {
+		System.out.println("test_Func01");
+		Java1_Whitebox j = new Java1_Whitebox();
+		double r = j.func01(100, 1, 0.0);
+		System.out.println(r);
+		assertEquals(101.0, r, 0);
+	}
+
+	@Test
+	public void test_Func02() {
+		System.out.println("test_Func02");
+		Java1_Whitebox j = new Java1_Whitebox();
+		double r = j.func01(50, 5, 0.0);
+		System.out.println(r);
+		assertEquals(55.0, r, 0);
+	}
+
+	@Test
+	public void test_Func03() {
+		System.out.println("test_Func03");
+		Java1_Whitebox j = new Java1_Whitebox();
+		double r = j.func01(-10, 9, 0.0);
+		System.out.println(r);
+		assertEquals(0.0, r, 0);
+	}
+
+	@Test
+	public void test_Func04() {
+		System.out.println("test_Func04");
+		Java1_Whitebox j = new Java1_Whitebox();
+		double r = j.func01(1, 100, 1.0);
+		System.out.println(r);
+		assertEquals(11.0, r, 0);
+	}
+
+	@Test
+	public void test_Func05() {
+		System.out.println("test_Func05");
+		Java1_Whitebox j = new Java1_Whitebox();
+		double r = j.func01(50, 10, 50.0);
+		System.out.println(r);
+		assertEquals(60.0, r, 0);
+	}
+
+	@Test
+	public void test_Func06() {
+		System.out.println("test_Func06");
+		Java1_Whitebox j = new Java1_Whitebox();
+		double r = j.func01(10, 10, -100.0);
+		System.out.println(r);
+		assertEquals(20.0, r, 0);
+	}
+}
---
+++ src/com/bw/test/Java2_BlackBox.java
@@ -0,0 +1,78 @@
+package com.bw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Java2_BlackBox {
+	String func(int x, int y) {
+		if (x > 10 && y > 100) {
+			return Integer.toString(x) + "x" + "y";
+		} else if (x < 0 && y < 10) {
+			return Integer.toString(y) + "xy" + "y";
+		} else {
+			return "x" + "y";
+		}
+	}
+
+	@Test
+	public void test_Func01() {
+		System.out.println("test_Func01");
+		Java2_BlackBox j = new Java2_BlackBox();
+		String r = j.func(50, 150);
+		System.out.println(r);
+		assertEquals("50xy", r);
+
+	}
+
+	@Test
+	public void test_Func02() {
+		System.out.println("test_Func02");
+		Java2_BlackBox j = new Java2_BlackBox();
+		String r = j.func(99, 101);
+		System.out.println(r);
+		assertEquals("99xy", r);
+
+	}
+
+	@Test
+	public void test_Func03() {
+		System.out.println("test_Func03");
+		Java2_BlackBox j = new Java2_BlackBox();
+		String r = j.func(1, -99);
+		System.out.println(r);
+		assertEquals("xy", r);
+
+	}
+
+	@Test
+	public void test_Func04() {
+		System.out.println("test_Func04");
+		Java2_BlackBox j = new Java2_BlackBox();
+		String r = j.func(-1, 9);
+		System.out.println(r);
+		assertEquals("9xyy", r);
+
+	}
+
+	@Test
+	public void test_Func05() {
+		System.out.println("test_Func05");
+		Java2_BlackBox j = new Java2_BlackBox();
+		String r = j.func(-99, 1);
+		System.out.println(r);
+		assertEquals("1xyy", r);
+
+	}
+
+	@Test
+	public void test_Func06() {
+		System.out.println("test_Func06");
+		Java2_BlackBox j = new Java2_BlackBox();
+		String r = j.func(-50, 5);
+		System.out.println(r);
+		assertEquals("5xyy", r);
+
+	}
+
+}
